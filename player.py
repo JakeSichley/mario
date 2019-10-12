8d80c6b33f3c0c5671a9be3547725cc7b90b3cb6
Hai Nguyen <54726078+hai-nguyen93@users.noreply.github.com>
2019-10-11 18:53:53 -0700

    Add files via upload

--- player.py
+++ player.py
@@ -22,54 +22,75 @@ class Player(Sprite):
         self.is_grounded = False
 
         # animations
+        self.facing_right = True
         self.walk_anim = Timer([pygame.image.load('images/player/walk1.bmp'),
+                                pygame.image.load('images/player/walk2.bmp'),
                                 pygame.image.load('images/player/walk3.bmp')])
         self.idle_anim = Timer([idle_image])
+        self.jump_anim = Timer([pygame.image.load('images/player/jump.bmp')])
         self.current_anim = self.idle_anim
 
     def update(self):
-        self.move()
-
-    def draw1(self, camera):
-        self.screen.blit(self.current_anim.imagerect(), camera.apply(self))
-
-    def draw(self):
-        self.screen.blit(self.current_anim.imagerect(), self.rect)
-
-    def move(self):
-        key_pressed = pygame.key.get_pressed()
-        left = key_pressed[K_a] or key_pressed[K_LEFT]
-        right = key_pressed[K_d] or key_pressed[K_RIGHT]
-        jump = key_pressed[K_SPACE]
-
-        if self.y + self.rect.height > 600:
+        # check if is grounded
+        if self.y + self.rect.height >= 600:
             self.y = 600 - self.rect.height
             self.is_grounded = True
             self.vel.y = 0
         else:
             self.is_grounded = False
 
+        self.move()
+
+    def draw1(self, camera):
+        if self.facing_right:
+            image = self.current_anim.imagerect()
+        else:
+            image = pygame.transform.flip(self.current_anim.imagerect(), True, False)
+        self.screen.blit(image, camera.apply(self))
+
+    def draw(self):
+        self.screen.blit(self.current_anim.imagerect(), self.rect)
+
+    def move(self):
+        # key inputs
+        key_pressed = pygame.key.get_pressed()
+        left = key_pressed[K_a] or key_pressed[K_LEFT]
+        right = key_pressed[K_d] or key_pressed[K_RIGHT]
+        jump = key_pressed[K_SPACE]
+
+        # gravity
         if not self.is_grounded:
             self.vel.y += self.gravity
             if self.vel.y >= 10:
                 self.vel.y = 10
+
+        # jump and move
         if jump and self.is_grounded:
             self.vel.y = -self.jump_power
         if left:  # move left
+            if self.facing_right:
+                self.facing_right = False
             if not self.camera.out_of_camera(self):
                 self.vel.x = -self.speed
             else:
                 self.vel.x = 0
         if right:  # move right
+            if not self.facing_right:
+                self.facing_right = True
             self.vel.x = self.speed
         if not (left or right):
             self.vel.x = 0
 
-        if self.vel.x > 0 or self.vel.x < 0:
-            self.current_anim = self.walk_anim
-        if self.vel.x == 0:
-            self.current_anim = self.idle_anim
+        # set animation
+        if not self.is_grounded:  # jump/fall animation
+            self.current_anim = self.jump_anim
+        else:
+            if self.vel.x > 0 or self.vel.x < 0:  # walk animation
+                self.current_anim = self.walk_anim
+            if self.vel.x == 0:
+                self.current_anim = self.idle_anim
 
+        # update coordinate
         self.y += self.vel.y
         self.x += self.vel.x
         self.rect.y = int(self.y)
