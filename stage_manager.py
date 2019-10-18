36197c067b45e06bc537bec5ca3ab7434c4c4a8a
Hai Nguyen <54726078+hai-nguyen93@users.noreply.github.com>
2019-10-18 11:35:29 -0700

    Add files via upload

--- stage_manager.py
+++ stage_manager.py
@@ -23,22 +23,23 @@ class StageManager:
             e.draw(camera)
 
     def load_stage(self, stage):  # character's position in txt file is left bot coordinate
-        item_img = pygame.image.load('images/Tile/box.png')
-        block_img = pygame.image.load('images/Tile/brick.png')
-        flower_img = pygame.image.load('images/Tile/flower.bmp')
-        with open('stage/stage1.txt', 'r') as f:
-            row = 1
-            for l in f:
-                col = 0
-                for c in l:
-                    if c == 'b':  # brick
-                        self.platforms.add(Tile(self.screen, 'brick', block_img, col*16, row*16))
-                    if c == 'i':  # item
-                        self.platforms.add(Tile(self.screen, 'item', item_img, col*16, row*16))
-                    if c == 'f':  # flower
-                        self.platforms.add(Tile(self.screen, 'flower', flower_img, col*16, row*16))
-                    if c == 'g':  # goomba
-                        self.enemies.add(Goomba(self.screen, col*16, row*16))
-                    col += 1
-                row += 1
-        f.close()
+        if stage == 1:
+            item_img = pygame.image.load('images/Tile/box.png')
+            block_img = pygame.image.load('images/Tile/brick.png')
+            flower_img = pygame.image.load('images/Tile/flower.bmp')
+            with open('stage/stage1.txt', 'r') as f:
+                row = 1
+                for l in f:
+                    col = 0
+                    for c in l:
+                        if c == 'b':  # brick
+                            self.platforms.add(Tile(self.screen, 'brick', block_img, col*16, row*16))
+                        if c == 'i':  # item
+                            self.platforms.add(Tile(self.screen, 'item', item_img, col*16, row*16))
+                        if c == 'f':  # flower
+                            self.platforms.add(Tile(self.screen, 'flower', flower_img, col*16, row*16))
+                        if c == 'g':  # goomba
+                            self.enemies.add(Goomba(self.screen, col*16, row*16))
+                        col += 1
+                    row += 1
+            f.close()
